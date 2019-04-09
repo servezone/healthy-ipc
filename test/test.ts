cc9949737892dfe2e5d40d05d3785bdd26f4528e
Phil Kunz <gitlab@philkunz.com>
2019-04-09 14:58:58 +0200

    fix(core): update

---
+++ test/test.ts
@@ -0,0 +1,8 @@
+import { expect, tap } from '@pushrocks/tapbundle';
+import * as healthyIpc from '../ts/index'
+
+tap.test('first test', async () => {
+  
+})
+
+tap.start()
---
+++ ts/index.ts
@@ -0,0 +1,11 @@
+import * as plugins from './healthy-ipc.plugins';
+
+export type THealthyTestFunction = () => 'healhty' | 'unhealthy';
+
+export interface IHealthTest {
+  healthFunc
+}
+
+export class HealthyIpc {
+  registerHealthFunction
+};
